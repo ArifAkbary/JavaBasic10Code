946ae24c1f1c44de4ba417092efb5f7c2f2f4496
ArifAkbary <arifakbary022@gmail.com>
2022-06-01 17:06:12 -0700

    contains 10 java classes

---
+++ src/Class7/WhileLoop.java
@@ -0,0 +1,24 @@
+package Class7;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		// print numbers form 100 to 50
+		
+		
+		 int d=10;
+		 while(d>1) {
+			 System.out.print(d +" ");
+			 d--;
+		 }
+
+	}
+
+}
+         
+         
+         
+
+
+
+
